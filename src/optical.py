0104c3aef0fccb7c3fe275da5897d009eab01d54
beykyle <beykyle@umich.edu>
2021-06-14 14:38:29 -0400

    Removed Nuc class

--- src/optical.py
+++ src/optical.py
@@ -6,13 +6,6 @@ import numpy as np
 This module provides simple optical potentials
 """
 
-class Nuclide:
-    def __init__(A : int , Z : int, r0=1.17, a=0.75 ):
-        self.A = A
-        self.Z = Z
-        self.R = r0 * np.pow(A,1./3.) # nuclear radisu [fm]
-        self.a = a # nuclear skin depth [fm]
-
 class simpleWoodSaxon:
     def __init__(V1 : float, V2 : float, V3: float, nuc : Nuclide):
         self.V = np.array([V1, V2, V3])
