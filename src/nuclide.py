10b6c9e4fd0f52feb36e7cf770103dad3eebfc23
beykyle <beykyle@umich.edu>
2021-06-15 15:33:27 -0400

    Add projectile class

--- src/nuclide.py
+++ src/nuclide.py
@@ -6,25 +6,24 @@ import numpy as np
 This module provides simple data structures from storing nuclear and projectile information used in optical calculations
 """
 
-amu2eV = 9.3149102E8 # eV/(amu * c^2)
-h      = 4.136E-15   # eV s
-c      = 2.998E23    # fm/s
-
-
-class Nuclide:
-    def __init__(A : int , Z : int, r0=1.17, a=0.75):
-        self.A = A
-        self.Z = Z
-        self.R = r0 * np.pow(A,1./3.) # nuclear radisu [fm]
-        self.a = a # nuclear skin depth [fm]
+amu2eV = 9.3149102E8          # eV/(amu * c^2)
+hbar   = 4.136E-15/(2*np.pi)  # eV s
+c      = 2.998E23             # fm/s
 
 class Projectile:
-    def __init__(mass):
-        self.m = mass # mass in amu
+    def __init__(self, A : int):
+        self.A = A # mass in amu
+
+class Nuclide:
+    def __init__(self, A : int , Z : int, r0=1.17, a=0.75):
+        self.A = A
+        self.Z = Z
+        self.R = r0 * np.power(A,1./3.) # nuclear radisu [fm]
+        self.a = a # nuclear skin depth [fm]
 
 def reducedMass_eV(proj : Projectile, target : Nuclide):
-    return (proj.mass * target.A)/(proj.mass + target.A) * amu2eV
+    return (proj.A * target.A)/(proj.A + target.A) * amu2eV
 
 def reducedh2m(proj : Projectile, target : Nuclide):
     m_reduced = reducedMass_eV(proj,target)
-    return h*h/(2*m_reduced) * c * c
+    return hbar*hbar/(2*m_reduced) * c * c
