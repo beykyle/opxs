71e653667b0ad08435ea3b4ab51ce89579a932a8
beykyle <beykyle@umich.edu>
2021-06-22 10:34:15 -0400

    Modified Bessel test

--- src/fg.py
+++ src/fg.py
@@ -9,30 +9,31 @@ from nuclide import Nuclide, reducedh2m
 This module numerically solves the radial Schroedinger's Equation in an arbitrary central potential using a finite differencing scheme
 
 TODO:
-    - test with 0 potential for Bessel function
-    - test with real potentials
     - extend to complex potentials
     - test with Koning-Delaroche optical model potentials
 """
+
+def norm(psi):
+    return psi/np.linalg.norm(psi)
 
 def solve(l : int, E : float, h2m : float, V : np.array, r : np.array, u : np.array):
     grid_size = V.size
     assert(r.size == grid_size)
     assert(u.size == grid_size)
-    deltaR = r[1:] - r[:-1]
-    h = deltaR[0]
+    deltar = r[1:] - r[:-1]
+    h = deltar[0]
 
     # set up potentials
     w      = (E - V) / h2m  - l * (l + 1) / r**2
-    w[0] = 0 # TODO numerical trick
+    w[0] = 0 # todo numerical trick
 
-    # finite difference: Fox-Goodwin scheme O(deltaR^4)
+    # finite difference: fox-goodwin scheme o(deltar^4)
     k = h**2/12
     for i in range(1,grid_size-1):
         u[i+1] = (2*u[i] - u[i-1] - k * (10 * w[i] * u[i] + w[i-1] * u[i-1])) /\
                    (1 + k * w[i+1] )
 
-    return u / np.sqrt(np.sum(u*u))
+    return norm(u)
 
 def plotPotential(r,V):
     # plot potential
@@ -65,7 +66,7 @@ def AlphaTest():
 
     # solve for multiple angular momenta
     for l in range(0,1):
-        E = -76.9036145E6 #TODO
+        E = -76.9036145E6
         u = solve(l,E,h2m,V,r,u)
         plt.plot(r, u, label=r"$\psi_{}(r)$".format(l))
 
@@ -77,32 +78,43 @@ def AlphaTest():
     plt.show()
 
 def BesselTest():
+    # this test doesn't work
     grid_sz = 1000
     Rmax    = 6.00 # fm
-    r       = np.linspace(0,Rmax,grid_sz)
+    r       = np.linspace(0,Rmax*2,grid_sz)
     proj    = Nuclide(4,2)
     targ    = Nuclide(4,2)
     h2m     = reducedh2m(proj, targ)
-    V0      = 1E15 # potential well w/ depth 1E9 eV
+    V0      = 1E10 # potential well w/ depth 1E9 eV
     V       = np.zeros(grid_sz)
-    #V[-1]   = V0
+    V[grid_sz//2:] = V0
 
     #plotPotential(r,V)
     # set up wavefuncton
     u = np.zeros(grid_sz)
-    u[0] = 1
+    u[0] = 0
     u[1] = 1
 
     # solve for multiple angular momenta
     lMax  = 1
     nperl = 2
     jn_zeros_sph = { 0 : [3.14, 6.28], 1 : [4.49, 7.73] }
+    colors = ['r' , 'g', 'b', 'k', 'r', 'g', 'b' 'k']
+    i = 0
     for l in range(0,lMax+1):
         allowed_k = np.array(jn_zeros_sph[l])/Rmax
         for n, k in enumerate(allowed_k):
+            i += 1
             E =  h2m * k**2
             u = solve(l,E,h2m,V,r,u)
-            plt.plot(r, u, label=r"$| {}{} \rangle$".format(n+1,l))
+            print(u)
+            u_anal = norm(sc.spherical_jn(l,k*r))
+            plt.scatter(r[:grid_sz//2], u[:grid_sz//2],
+                    label=r"$| {}{} \rangle$: Fox-Goodwin".format(n+1,l),
+                    marker='*', color=colors[i])
+            plt.plot(r[:grid_sz//2], u_anal[:grid_sz//2],
+                    label=r"$j_{}(k_{}r)$".format(l,n+1),
+                    color=colors[i])
 
     # plot resulting wavefunction
     #plt.plot(r, u*u/np.max(u), label=r"$|\psi_{}(r)|^2$".format(l))
@@ -112,7 +124,7 @@ def BesselTest():
     plt.show()
 
 if __name__ == "__main__":
-    print("Running 0 potential for Bessel function solutions")
-    BesselTest()
     print("Running alpha-alpha reaction example from TPOTPC, ch. 13")
     AlphaTest()
+    print("Bessel test")
+    BesselTest()
