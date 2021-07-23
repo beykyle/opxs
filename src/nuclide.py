a37ad9d9e7e26ab7a37656ccf422fbe65157e899
beykyle <beykyle@umich.edu>
2021-07-23 10:16:14 -0400

    Pull in nuc data, add Jpi

--- src/nuclide.py
+++ src/nuclide.py
@@ -1,6 +1,11 @@
 #! /usr/bin/python3
 
 import numpy as np
+import sys
+sys.path.append("/home/beykyle/umich/nuclide-data")
+import nuclide_data
+
+
 
 """
 This module provides simple data structures from storing nuclear and projectile information used in optical calculations
@@ -13,27 +18,31 @@ e      = 1.439964548          # MeV-fm
 
 class Projectile():
     def __init__(self, A : int, Z = 0):
-        self.A = A
-        self.Z = Z
-        self.m = Nuclide(A,Z).mass()
+        self.A   = A
+        self.Z   = Z
+        self.m   = Nuclide(A,Z).mass()
+        self.Jpi = Nuclide(A,Z).spin()
 
 class Neutron(Projectile):
     def __init__(self):
-        self.A = 1
-        self.Z = 0
-        self.m = 1.008665
+        self.A    = 1
+        self.Z    = 0
+        self.m    = 1.008665
+        self.Jpi  = 1/2,True
 
 class Proton(Projectile):
     def __init__(self):
-        self.A = 0
-        self.Z = 1
-        self.m = 1.0072764
+        self.A    = 0
+        self.Z    = 1
+        self.m    = 1.0072764
+        self.Jpi  = 1/2,True
 
-class Nuclide:
+class Nuclide(Projectile):
     def __init__(self, A : int , Z : int, r0=1.17, a=0.75):
         self.A = A
         self.Z = Z
-        self.m = self.mass()
+        self.m   = self.mass()
+        self.Jpi = self.spin()
         self.R = r0 * np.power(A,1./3.) # nuclear radius [fm]
         self.a = a                      # nuclear skin depth [fm]
 
@@ -59,6 +68,11 @@ class Nuclide:
 
     def compound(self, proj : Projectile):
         return Nuclide(self.A + proj.A, self.Z + proj.Z)
+
+    """ returns (J,parity) in ground state from shell model. pi = (True,False) for (even,odd) """
+    def spin(self):
+        ## TODO pull in nuclide_data
+        return 0, True
 
 def reducedMass_eV(proj : Projectile, target : Nuclide):
     return (proj.m * target.m)/(proj.m + target.m) * amu2eV
